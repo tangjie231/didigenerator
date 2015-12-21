93d7014064034bfa976c7db87eee97b47611c530
tangjie <280830926@qq.com>
2015-12-21 18:57:22 +0800

    change

---
+++ app/components/service/pagerService.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo on 2015/12/21.
+ */
+define(["angular"],function(angular){
+    var pager = angular.module("pager", []);
+    pager.service("pagerService", function () {
+        var service = {
+            sayHello:function(name){
+                console.log("hello "+name);
+            }
+        }
+        return service;
+    });
+
+    return pager;
+});
